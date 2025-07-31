44117e71cd4e35d3fb015e896db20d44c301a7e9
hwimin <whimin0319@gmail.com>
2025-07-31 11:36:23 +0900

    14657 휘민 -ing

---
+++ Tree/[백준]14675_단절점과_단절선/14675_휘민.py
@@ -0,0 +1,20 @@
+# ing ... 
+
+N = int(input())
+adj = [[] for _ in range(N+1)]
+edges = []
+
+for _ in range(N):
+    a, b = map(int, input().split())
+    adj[a].append(b)
+    adj[b].append(a)
+    edges.append((a, b))
+
+Q = int(input())
+for _ in range(Q):
+    t, k = map(int, input().split())
+    if t == 2:
+        print('yes')
+
+    else:
+        if edges[k]
