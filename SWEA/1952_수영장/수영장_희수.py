cab922a4c18cdd5e8008aafbed9b46de55e75063
이희수 <leeheesoo0624@gamil.com>
2025-08-14 13:49:58 +0900

    희수영장 주석달음

--- SWEA/1952_수영장/수영장_희수.py
+++ SWEA/1952_수영장/수영장_희수.py
@@ -1,20 +1,20 @@
 def dfs(month_idx, current_cost):
-    global min_cost
+    global min_cost #최소 가격 저장할 변수
 
-    if month_idx >= 12 :
+    if month_idx >= 12 : # month_idx : 0 ~ 11
         min_cost = min(min_cost, current_cost)
         return
 
-    dfs(12, year)
-    dfs(month_idx + 3, current_cost + three_month)
-    dfs(month_idx + 1, current_cost + one_month)
-    dfs(month_idx + 1, current_cost + day * plans[month_idx])
+    dfs(12, year) # 1년 요금
+    dfs(month_idx + 3, current_cost + three_month) # 3개월 요금
+    dfs(month_idx + 1, current_cost + one_month) # 1개월 요금
+    dfs(month_idx + 1, current_cost + day * plans[month_idx]) # 1일 요금으로 한 달치
 
 t = int(input())
 for tc in range(1, t+1) :
     day, one_month, three_month, year = map(int, input().split())
     plans = list(map(int, input().split()))
-    min_cost = 10 * 365
+    min_cost = day * 365 # 최대 값 저장
 
     dfs(0, 0)
     print(f'#{tc} {min_cost}')
