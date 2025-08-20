533368d9a3fbc03679d7adeb1f03c144c7715d2c
한가희 <jess5858@naver.com>
2025-08-20 11:05:57 +0900

    배열돌리기 주석추가

--- A형대비/17406_배열돌리기4/17406_희수.py
+++ A형대비/17406_배열돌리기4/17406_희수.py
@@ -1,11 +1,12 @@
+# 배열 회전
 def rotate(r, c, s, arr):
-    new_arr = [row[:] for row in arr]
-    left = c-s-1
-    up = r-s-1
-    right = c+s-1
-    down = r+s-1
-    mi = 0
-    while left < right and up < down:
+    new_arr = [row[:] for row in arr] # 새 배열 복사
+    left = c-s-1 # 왼쪽 끝
+    up = r-s-1 # 위쪽 끝
+    right = c+s-1 # 오른쪽 끝
+    down = r+s-1 # 아래쪽 끝
+    mi = 0 # 회전 대상 점점 작아지게 만들 숫자
+    while left < right and up < down: # 회전할 수 있을 때까지 배열 돌림
         new_arr[up][left + 1 : right + 1] = arr[up][left : right]
         new_arr[down][left : right] = arr[down][left + 1 : right + 1]
         for i in range(2 * s - mi):
@@ -15,17 +16,18 @@ def rotate(r, c, s, arr):
         right -= 1
         up += 1
         down -= 1
-        mi += 2
+        mi += 2 # 배열 크기 줄이기
     return new_arr
 
+# 회전 연산 순서 순열 + 최소값 찾기
 def dfs(arr):
     global min_sum
-    if len(visited) == k:
-        for row in arr:
-            min_sum = min(min_sum, sum(row))
+    if len(visited) == k: # 모든 연산 수행하면
+        for row in arr: # 배열 행별로 계산
+            min_sum = min(min_sum, sum(row)) # 최소값 저장
         return
 
-    for i in range(k):
+    for i in range(k): # 회전 연산 순열
         if i not in visited:
             visited.append(i)
             r, c, s = info[i]
@@ -35,8 +37,11 @@ def dfs(arr):
         
 
 n, m, k = map(int, input().split())
+# 열, 행, 회전 연산 개수
 arra = [list(map(int, input().split())) for _ in range(n)]
+# 배열
 info = [list(map(int, input().split())) for _ in range(k)]
+# 회전 연산 정보 [r, c, s]
 visited = []
 min_sum = float('inf')
 
